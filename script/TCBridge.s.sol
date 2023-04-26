e80e0587a2fa6c24ff0b08576092e3304b8825df
180945 <180945@trustless.computer>
2023-04-26 19:14:45 +0700

    added optimization

--- script/TCBridge.s.sol
+++ script/TCBridge.s.sol
@@ -18,10 +18,14 @@ contract TCScript is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         // deploy bridge tc contract
-        address[] memory owners = new address[](3);
-        owners[0] = address(0x9699b31b25D71BDA4819bBe66244E9130cEE62b7);
-        owners[1] = address(0x54b3DBA467C9Dbb916EF4D6AedaFa19C4Fef8258);
-        owners[2] = address(0xD7d93b7fa42b60b6076f3017fCA99b69257A912D);
+        address[] memory owners = new address[](7);
+        owners[0] = address(0xF165DBE65127feca0abbD7d734B4a2a3c3C6aA84);
+        owners[1] = address(0xd1950Ce1cd947B0F0378c9eB9618b705A13539A2);
+        owners[2] = address(0x2150E0F033f2F8E8c13Fe2089A0cB399521604FF);
+        owners[3] = address(0x9B9e024D6C2a9c3eF921497FbE53c57a851321cd);
+        owners[4] = address(0xd85f5f63E83bDec8a92dd3C7f7FaEFE671024d85);
+        owners[5] = address(0xD898eE20D858da55A7A58D1069BD47be234dC50f);
+        owners[6] = address(0xB39310E75b773876dBa6006aDeE116BC40363994);
 
         Safe impl = new Safe();
         Safe safe = Safe(payable(address(new TransparentUpgradeableProxy(
