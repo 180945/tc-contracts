2dbb71ef413cba491d91150ac028ba233cfebe00
180945 <180945@trustless.computer>
2023-04-28 17:38:34 +0700

    config mainnet

--- script/TCBridge.s.sol
+++ script/TCBridge.s.sol
@@ -10,7 +10,7 @@ import "../src/TCBridge.sol";
 contract TCScript is Script {
     address upgradeAddress;
     function setUp() public {
-        upgradeAddress = 0x8F6A5136F8f4674e432B63D640Da04c7DB663c06;
+        upgradeAddress = 0xE7143319283D0b5b234AEA046769D40bee5C6D43;
     }
 
     function run() public {
@@ -19,13 +19,13 @@ contract TCScript is Script {
 
         // deploy bridge tc contract
         address[] memory owners = new address[](7);
-        owners[0] = address(0xF165DBE65127feca0abbD7d734B4a2a3c3C6aA84);
-        owners[1] = address(0xd1950Ce1cd947B0F0378c9eB9618b705A13539A2);
-        owners[2] = address(0x2150E0F033f2F8E8c13Fe2089A0cB399521604FF);
-        owners[3] = address(0x9B9e024D6C2a9c3eF921497FbE53c57a851321cd);
-        owners[4] = address(0xd85f5f63E83bDec8a92dd3C7f7FaEFE671024d85);
-        owners[5] = address(0xD898eE20D858da55A7A58D1069BD47be234dC50f);
-        owners[6] = address(0xB39310E75b773876dBa6006aDeE116BC40363994);
+        owners[0] = address(0x142d09aCeA7b067639c8Dc4CF55fe907c5706c80);
+        owners[1] = address(0x48519f5d37d4acE18011b5470EaaFDAf400079Cb);
+        owners[2] = address(0x6c4ABD94C6eF3A8B092a673bE7D069F6A3a0c764);
+        owners[3] = address(0xa32D154D0824C4d898b5A3b054E4aa0346322724);
+        owners[4] = address(0x2550f37641FFB5e4928052aDDD788Cd8514d16a7);
+        owners[5] = address(0xe64F53d154A25498870202aceff40A4344385a18);
+        owners[6] = address(0x7d32913ad31Cdd2DAfA4eB024eF8fa0A3E6F9D95);
 
         Safe impl = new Safe();
         Safe safe = Safe(payable(address(new TransparentUpgradeableProxy(
