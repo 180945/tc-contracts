edb3e21c272e58f949c3753910f02a2be327f6a0
180945 <180945@trustless.computer>
2023-08-03 13:43:19 +0700

    update game policy

--- src/gamePlatform/ttt/ttt.sol
+++ src/gamePlatform/ttt/ttt.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import { Elo } from "@tc/Elo.sol";
 
-contract TTT {
+contract Chess {
     /**
      * @notice Input the current elo and game result will get new output elo
      * @return
@@ -42,29 +42,14 @@ contract TTT {
 
     // @notice this function evaluate the TC max match creator can bet
     // address 0x00 for native token
-    //>= 2000: Expert: 6.4 ($80)
-    //1800–1999: Class A: 3.2 ($40)
-    //1600–1799: Class B: 1.6 ($20)
-    //1400–1599: Class C: 0,8 ($10)
-    //1200–1399: Class D: 0,4 ($5)
-    //1000–1199: Class E: 0,2 ($2.5)
+    //>= 800: Expert: 100 TC
     function maxCanBet(address player, address token, int elo) external pure returns(uint) {
         player;
         token;
 
         uint max;
-        if (elo >= 1000 && elo < 1200) {
-            max = 2 * 1e17;
-        } else if (elo >= 1200 && elo < 1400) {
-            max = 4 * 1e17;
-        } else if (elo >= 1400 && elo < 1600) {
-            max = 8 * 1e17;
-        } else if (elo >= 1600 && elo < 1800) {
-            max = 16 * 1e17;
-        } else if (elo >= 1800 && elo < 2000) {
-            max = 32 * 1e17;
-        } else if (max >= 2000) {
-            max = 64 * 1e17;
+        if (elo >= 800) {
+            max = 100 ether;
         }
 
         return max;
